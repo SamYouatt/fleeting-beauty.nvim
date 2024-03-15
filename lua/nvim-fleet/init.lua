b1d9102c0b8aea7135bfc904cf764e4051dc3b61
SamYouatt <sdyouatt@gmail.com>
2024-03-15 18:52:42 +0000

    start creating plugin

---
+++ lua/nvim-fleet/init.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+local function get_colours()
+    local groups = {
+        -- This is just any normal space so should be easy to see if its loaded
+        Normal = { fg = #000000, bg = #BBBBBB }
+    }
+
+    return groups
+end
+
+M.setup = function()
+    vim.notify("Setting up nvim-fleet", vim.log.levels.ERROR)
+end
+
+M.load = function()
+    vim.notify("Loaded fleet theme", vim.log.levels.ERROR)
+
+    local groups = get_colours()
+
+    for group, colours in pair(groups) do
+        vim.api.nvim_set_hl(0, group, colours)
+    end
+end
+
+return M
