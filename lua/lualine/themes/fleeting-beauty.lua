8239aac7dc37e654ed6d0e7a0edd15871b34601c
SamYouatt <sdyouatt@gmail.com>
2024-03-17 16:20:30 +0000

    still working on getting lualine working

--- lua/lualine/themes/fleeting-beauty.lua
+++ lua/lualine/themes/fleeting-beauty.lua
@@ -1,1 +1,41 @@
-return require "fleeting-beauty.lualine" "bongle"
+local colours = require("fleeting-beauty.palettes.dark")
+local fleeting_beauty = {}
+
+fleeting_beauty.normal = {
+    a = { bg = colours.blue, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.blue },
+    c = { bg = "NONE", fg = colours.text },
+}
+
+fleeting_beauty.insert = {
+    a = { bg = colours.green, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.green },
+}
+
+fleeting_beauty.terminal = {
+    a = { bg = colours.green, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.green },
+}
+
+fleeting_beauty.command = {
+    a = { bg = colours.peach, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.peach },
+}
+
+fleeting_beauty.visual = {
+    a = { bg = colours.mauve, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.mauve },
+}
+
+fleeting_beauty.replace = {
+    a = { bg = colours.red, fg = colours.base, gui = "bold" },
+    b = { bg = colours.alt_base, fg = colours.red },
+}
+
+fleeting_beauty.inactive = {
+    a = { bg = C.alt_base, fg = C.blue },
+    b = { bg = C.alt_base, fg = C.surface1, gui = "bold" },
+    c = { bg = C.alt_base, fg = C.overlay0 },
+}
+
+return fleeting_beauty
