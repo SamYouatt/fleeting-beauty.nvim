60e768b5594e237e72bccb5ba1122a528bc31d54
SamYouatt <sdyouatt@gmail.com>
2024-03-17 11:11:43 +0000

    most of the way on syntax now, including sem tokens

--- lua/fleeting-beauty/groups/dark/syntax.lua
+++ lua/fleeting-beauty/groups/dark/syntax.lua
@@ -2,41 +2,41 @@ local M = {}
 
 function M.get()
     return {
-        Comment = { fg = C.subtext1 },  -- just comments
-        SpecialComment = {},            -- special things inside a comment
-        Constant = { fg = C.lavender },   -- (preferred) any constant
-        String = { fg = C.pink },       -- a string constant: "this is a string"
-        Character = { fg = C.teal },    --  a character constant: 'c', '\n'
-        Number = { fg = C.yellow },     --   a number constant: 234, 0xff
-        Float = { link = "Number" },    --    a floating point constant: 2.3e10
-        Boolean = { fg = C.teal },      --  a boolean constant: TRUE, false
-        Identifier = { fg = C.text },   -- (preferred) any variable name
-        Function = { fg = C.yellow },   -- function name (also: methods for classes)
-        Statement = { fg = C.teal },    -- (preferred) any statement
-        Conditional = { fg = C.teal },  --  if, then, else, endif, switch, etc.
-        Repeat = { fg = C.teal },       --   for, do, while, etc.
-        Label = { fg = C.teal },        --    case, default, etc.
-        Operator = { fg = C.text },     -- "sizeof", "+", "*", etc.
-        Keyword = { fg = C.teal },      --  any other keyword
-        Exception = { fg = C.teal },    --  try, catch, throw
+        Comment = { fg = C.subtext1 },        -- just comments
+        SpecialComment = {},                  -- special things inside a comment
+        Constant = { fg = C.lavender },       -- (preferred) any constant
+        String = { fg = C.pink },             -- a string constant: "this is a string"
+        Character = { fg = C.teal },          --  a character constant: 'c', '\n'
+        Number = { fg = C.yellow },           --   a number constant: 234, 0xff
+        Float = { link = "Number" },          --    a floating point constant: 2.3e10
+        Boolean = { fg = C.teal },            --  a boolean constant: TRUE, false
+        Identifier = { fg = C.text },         -- (preferred) any variable name
+        Function = { fg = C.yellow },         -- function name (also: methods for classes)
+        Statement = { fg = C.teal },          -- (preferred) any statement
+        Conditional = { fg = C.teal },        --  if, then, else, endif, switch, etc.
+        Repeat = { fg = C.teal },             --   for, do, while, etc.
+        Label = { fg = C.teal },              --    case, default, etc.
+        Operator = { fg = C.text },           -- "sizeof", "+", "*", etc.
+        Keyword = { fg = C.teal },            --  any other keyword
+        Exception = { fg = C.teal },          --  try, catch, throw
 
-        PreProc = {},                   -- (preferred) generic Preprocessor
-        Include = { fg = C.teal },      --  preprocessor #include
-        Define = {},                    -- preprocessor #define
-        Macro = {},                     -- same as Define
-        PreCondit = {},                 -- preprocessor #if, #else, #endif, etc.
+        PreProc = { fg = C.pink },                         -- (preferred) generic Preprocessor
+        Include = { fg = C.teal },            --  preprocessor #include
+        Define = {},                          -- preprocessor #define
+        Macro = { fg = C.lavender },                           -- same as Define
+        PreCondit = { fg = C.teal },                       -- preprocessor #if, #else, #endif, etc.
 
-        StorageClass = { fg = C.blue }, -- static, register, volatile, etc.
-        Structure = { fg = C.blue },    --  struct, union, enum, etc.
-        Special = {},                   -- (preferred) any special symbol
-        Type = { fg = C.blue },         -- (preferred) int, long, char, etc.
-        Typedef = {},                   --  A typedef
-        SpecialChar = {},               -- special character in a constant
-        Tag = { fg = C.sapphire },      -- you can use CTRL-] on this
-        Delimiter = {},                 -- character that needs attention
-        Debug = {},                     -- debugging statements
+        StorageClass = { fg = C.blue },       -- static, register, volatile, etc.
+        Structure = { fg = C.blue },          --  struct, union, enum, etc.
+        Special = { link = "Character" },     -- (preferred) any special symbol
+        Type = { fg = C.blue },               -- (preferred) int, long, char, etc.
+        Typedef = {},                         --  A typedef
+        SpecialChar = { link = "Character" }, -- special character in a constant
+        Tag = { fg = C.sapphire },            -- you can use CTRL-] on this
+        Delimiter = { fg = C.text },   -- character that needs attention
+        Debug = {},                           -- debugging statements
 
-        Underlined = {},                -- (preferred) text that stands out, HTML links
+        Underlined = {},                      -- (preferred) text that stands out, HTML links
         Bold = {},
         Italic = {},
         -- ("Ignore", below, may be invisible...)
@@ -68,7 +68,7 @@ function M.get()
         diffOldFile = {},
         diffNewFile = {},
         diffFile = {},
-        diffLine = {},
+        diffLine = { fg = C.subtext1 },
         diffIndexLine = {},
         DiffAdd = {},    -- diff mode: Added line |diff.txt|
         DiffChange = {}, -- diff mode: Changed line |diff.txt|
--- lua/fleeting-beauty/init.lua
+++ lua/fleeting-beauty/init.lua
@@ -7,8 +7,9 @@ local function get_colours()
     theme.editor = require("fleeting-beauty.groups.dark.editor").get()
     theme.syntax = require("fleeting-beauty.groups.dark.syntax").get()
     theme.treesitter = require("fleeting-beauty.groups.dark.treesitter").get()
+    theme.semantic_tokens = require("fleeting-beauty.groups.dark.semantic_tokens").get()
 
-    local groups = vim.tbl_deep_extend("keep", theme.treesitter, theme.syntax, theme.editor)
+    local groups = vim.tbl_deep_extend("keep", theme.semantic_tokens, theme.treesitter, theme.syntax, theme.editor)
 
     return groups
 end
