1847117866b9d88fdbb43f7a8d651865877df3df
SamYouatt <sdyouatt@gmail.com>
2024-03-23 19:13:25 +0000

    dark: make telescope background normal

--- lua/fleety/groups/dark/telescope.lua
+++ lua/fleety/groups/dark/telescope.lua
@@ -2,10 +2,12 @@ local M = {}
 
 function M.get()
     return {
+        TelescopeNormal = { fg = C.text, bg = C.alt_base },
         TelescopeBorder = { link = "FloatBorder" },
         TelescopeSelectionCaret = { fg = C.blue },
         TelescopeSelection = { fg = C.text, bg = C.navy },
         TelescopeMatching = { fg = C.text, bg = C.burnt_orange },
+        TelescopePreviewNormal = { bg = C.base },
     }
 end
 
